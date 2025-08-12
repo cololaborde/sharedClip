6aa072594e03d8c5d03b09736764b46610bf4c8b
cololaborde <cololaborde@gmail.com>
2025-08-11 22:44:22 -0300

    use current path

--- invoke.sh
+++ invoke.sh
@@ -1,12 +1,22 @@
+current_path=$(pwd)
+
 #!/bin/bash
-exec >> /home/colo/Documentos/sharedClip/log.txt 2>&1
+exec >> "$current_path/log.txt" 2>&1
 
 if [ -z "$1" ]; then
   echo "No argument supplied"
   exit 1
 fi
 
-source /home/colo/Documentos/sharedClip/venv/bin/activate
-/home/colo/Documentos/sharedClip/venv/bin/python /home/colo/Documentos/sharedClip/invoke.py "$1"
+#check if venv exist
+if [ ! -d "$current_path/venv" ]; then
+  python3 -m venv "$current_path/venv"
+  source "$current_path/venv/bin/activate"
+  pip install -r "$current_path/requirements.txt"
+  deactivate
+fi
+
+source "$current_path/venv/bin/activate"
+"$current_path/venv/bin/python" "$current_path/invoke.py" "$1"
 deactivate
 exit 0
